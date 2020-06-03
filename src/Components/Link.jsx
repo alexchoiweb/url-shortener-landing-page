371d918c9eaea779bcf955c1e6bad407451f83ab
alexchoiweb <alexchoiweb@gmail.com>
2020-06-02 19:25:37 -0700

    links responsive done

--- src/Components/Link.jsx
+++ src/Components/Link.jsx
@@ -10,18 +10,20 @@ export default function Link(props) {
   return (
     <div className="Link">
       <p className="Link__originalLink">{props.urls.originalLink}</p>
-      <p className="Link__shortLink">{props.urls.shortLink}</p>
-      <input
-        readOnly
-        type="text"
-        // style={{ display: "none" }}
-        style={{ position: "absolute", left: "-1000px" }}
-        id={props.id}
-        value={props.urls.shortLink}
-      ></input>
-      <button className="Link__copyButton button-cta" onClick={copy}>
-        Copy
-      </button>
+      <div className="Link__shortLinkContainer">
+        <p className="Link__shortLink">{props.urls.shortLink}</p>
+        <input
+          readOnly
+          type="text"
+          // style={{ display: "none" }}
+          style={{ position: "absolute", left: "-1000px" }}
+          id={props.id}
+          value={props.urls.shortLink}
+        ></input>
+        <button className="Link__copyButton button-cta" onClick={copy}>
+          Copy
+        </button>
+      </div>
     </div>
   );
 }
